3e00f1bcc86ccd8e3e92bd09ef5bf5528b1b8748
nikouk <nikouk@nikouk.co.uk>
2017-12-22 00:31:20 +0000

    new

--- scripts/form-u951.php
+++ scripts/form-u951.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => '',
-		'to' => ''
+		'from' => 'niko@nikouk.co.uk',
+		'to' => 'niko@nikouk.co.uk'
 	),
 	'fields' => array(
 		'custom_U952' => array(
