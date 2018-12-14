2190abb64588e23ad9ce519b6bc777ae07878d85
nikouk <nikouk@nikouk.co.uk>
2018-12-14 17:40:50 +0000

    n

---
+++ js/index.20181214174007.js
@@ -0,0 +1,66 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+function em1(){var c="jogpAojlpvl/dp/vl";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=1200){$('.js').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-219.png' : 'images/pasted-image-146.png') : 'images/pasted-image-73.png');
+$('.js-4').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-69.png' : 'images/pasted-image-46.png') : 'images/pasted-image-23.png');
+$('.js-5').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-81.png' : 'images/pasted-image-54.png') : 'images/pasted-image-27.png');
+$('.js-6').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-96.png' : 'images/pasted-image-64.png') : 'images/pasted-image-32.png');
+var a='data-src'; if($('.js-8 .slide0').hasAttr('src')) { a='src'; } $('.js-8 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-387.png' : 'images/77d-258.png') : 'images/77d-129.png');
+var a='data-src'; if($('.js-8 .slide1').hasAttr('src')) { a='src'; } $('.js-8 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-387.png' : 'images/77d-258.png') : 'images/77d-129.png');
+var a='data-src'; if($('.js-8 .slide2').hasAttr('src')) { a='src'; } $('.js-8 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/k1000-387.png' : 'images/k1000-258.png') : 'images/k1000-129.png');
+var a='data-src'; if($('.js-8 .slide3').hasAttr('src')) { a='src'; } $('.js-8 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-387.png' : 'images/77d-258.png') : 'images/77d-129.png');
+var a='data-src'; if($('.js-8 .slide4').hasAttr('src')) { a='src'; } $('.js-8 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-387.png' : 'images/77d-258.png') : 'images/77d-129.png');
+var a='data-src'; if($('.js-8 .slide5').hasAttr('src')) { a='src'; } $('.js-8 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-387.png' : 'images/77d-258.png') : 'images/77d-129.png');
+var a='data-src'; if($('.js-9 .slide0').hasAttr('src')) { a='src'; } $('.js-9 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-387.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-258.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-129.png');
+var a='data-src'; if($('.js-9 .slide1').hasAttr('src')) { a='src'; } $('.js-9 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/canon-ef-70-200-mm-f2-8-l-is-ii-387.png' : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-258.png') : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-129.png');
+var a='data-src'; if($('.js-9 .slide2').hasAttr('src')) { a='src'; } $('.js-9 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/50mm-387.png' : 'images/50mm-258.png') : 'images/50mm-129.png');
+var a='data-src'; if($('.js-9 .slide3').hasAttr('src')) { a='src'; } $('.js-9 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/canon-ef-70-200-mm-f2-8-l-is-ii-387.png' : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-258.png') : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-129.png');
+var a='data-src'; if($('.js-9 .slide4').hasAttr('src')) { a='src'; } $('.js-9 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-387.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-258.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-129.png');
+var a='data-src'; if($('.js-9 .slide5').hasAttr('src')) { a='src'; } $('.js-9 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-387.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-258.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-129.png');
+var a='data-src'; if($('.js-11 .slide0').hasAttr('src')) { a='src'; } $('.js-11 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-15-1392.jpg' : 'images/motivenov-15-928.jpg') : 'images/motivenov-15-464.jpg');
+var a='data-src'; if($('.js-11 .slide1').hasAttr('src')) { a='src'; } $('.js-11 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/22sep-65-1392.jpg' : 'images/22sep-65-928.jpg') : 'images/22sep-65-464.jpg');
+var a='data-src'; if($('.js-11 .slide2').hasAttr('src')) { a='src'; } $('.js-11 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/r001-006-1392.jpg' : 'images/r001-006-928.jpg') : 'images/r001-006-464.jpg');
+var a='data-src'; if($('.js-11 .slide3').hasAttr('src')) { a='src'; } $('.js-11 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/22sep-19-1392.jpg' : 'images/22sep-19-928.jpg') : 'images/22sep-19-464.jpg');
+var a='data-src'; if($('.js-11 .slide4').hasAttr('src')) { a='src'; } $('.js-11 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-59-1392.jpg' : 'images/motivenov-59-928.jpg') : 'images/motivenov-59-464.jpg');
+var a='data-src'; if($('.js-11 .slide5').hasAttr('src')) { a='src'; } $('.js-11 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-74-1392.jpg' : 'images/motivenov-74-928.jpg') : 'images/motivenov-74-464.jpg');}else{$('.js').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-99.png' : 'images/pasted-image-66.png') : 'images/pasted-image-33.png');
+$('.js-4').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-42.png' : 'images/pasted-image-28.png') : 'images/pasted-image-14.png');
+$('.js-5').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-51.png' : 'images/pasted-image-34.png') : 'images/pasted-image-17.png');
+$('.js-6').attr('src', (dpi>1) ? ((dpi>2) ? 'images/pasted-image-63.png' : 'images/pasted-image-42-1.png') : 'images/pasted-image-21.png');
+var a='data-src'; if($('.js-8 .slide0').hasAttr('src')) { a='src'; } $('.js-8 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-180.png' : 'images/77d-120.png') : 'images/77d-60.png');
+var a='data-src'; if($('.js-8 .slide1').hasAttr('src')) { a='src'; } $('.js-8 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-180.png' : 'images/77d-120.png') : 'images/77d-60.png');
+var a='data-src'; if($('.js-8 .slide2').hasAttr('src')) { a='src'; } $('.js-8 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/k1000-180.png' : 'images/k1000-120.png') : 'images/k1000-60.png');
+var a='data-src'; if($('.js-8 .slide3').hasAttr('src')) { a='src'; } $('.js-8 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-180.png' : 'images/77d-120.png') : 'images/77d-60.png');
+var a='data-src'; if($('.js-8 .slide4').hasAttr('src')) { a='src'; } $('.js-8 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-180.png' : 'images/77d-120.png') : 'images/77d-60.png');
+var a='data-src'; if($('.js-8 .slide5').hasAttr('src')) { a='src'; } $('.js-8 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/77d-180.png' : 'images/77d-120.png') : 'images/77d-60.png');
+var a='data-src'; if($('.js-9 .slide0').hasAttr('src')) { a='src'; } $('.js-9 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-177.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-118.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-59.png');
+var a='data-src'; if($('.js-9 .slide1').hasAttr('src')) { a='src'; } $('.js-9 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/canon-ef-70-200-mm-f2-8-l-is-ii-177.png' : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-118.png') : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-59.png');
+var a='data-src'; if($('.js-9 .slide2').hasAttr('src')) { a='src'; } $('.js-9 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/50mm-177.png' : 'images/50mm-118.png') : 'images/50mm-59.png');
+var a='data-src'; if($('.js-9 .slide3').hasAttr('src')) { a='src'; } $('.js-9 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/canon-ef-70-200-mm-f2-8-l-is-ii-177.png' : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-118.png') : 'images/canon-ef-70-200-mm-f2-8-l-is-ii-59.png');
+var a='data-src'; if($('.js-9 .slide4').hasAttr('src')) { a='src'; } $('.js-9 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-177.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-118.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-59.png');
+var a='data-src'; if($('.js-9 .slide5').hasAttr('src')) { a='src'; } $('.js-9 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-177.png' : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-118.png') : 'images/0570c005_canon-ef-50mm-f-1.8-stm_1-59.png');
+var a='data-src'; if($('.js-11 .slide0').hasAttr('src')) { a='src'; } $('.js-11 .slide0').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-15-507.jpg' : 'images/motivenov-15-338.jpg') : 'images/motivenov-15-169.jpg');
+var a='data-src'; if($('.js-11 .slide1').hasAttr('src')) { a='src'; } $('.js-11 .slide1').attr(a, (dpi>1) ? ((dpi>2) ? 'images/22sep-65-507.jpg' : 'images/22sep-65-338.jpg') : 'images/22sep-65-169.jpg');
+var a='data-src'; if($('.js-11 .slide2').hasAttr('src')) { a='src'; } $('.js-11 .slide2').attr(a, (dpi>1) ? ((dpi>2) ? 'images/r001-006-507.jpg' : 'images/r001-006-338.jpg') : 'images/r001-006-169.jpg');
+var a='data-src'; if($('.js-11 .slide3').hasAttr('src')) { a='src'; } $('.js-11 .slide3').attr(a, (dpi>1) ? ((dpi>2) ? 'images/22sep-19-507.jpg' : 'images/22sep-19-338.jpg') : 'images/22sep-19-169.jpg');
+var a='data-src'; if($('.js-11 .slide4').hasAttr('src')) { a='src'; } $('.js-11 .slide4').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-59-507.jpg' : 'images/motivenov-59-338.jpg') : 'images/motivenov-59-169.jpg');
+var a='data-src'; if($('.js-11 .slide5').hasAttr('src')) { a='src'; } $('.js-11 .slide5').attr(a, (dpi>1) ? ((dpi>2) ? 'images/motivenov-74-507.jpg' : 'images/motivenov-74-338.jpg') : 'images/motivenov-74-169.jpg');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+$('.js-8 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: false,infinite: true,autoplay: true,pauseOnHover: false,autoplaySpeed: 2000});$('.js-9 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: false,infinite: true,autoplay: true,pauseOnHover: false,autoplaySpeed: 2000});$('.js-11 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: false,infinite: true,autoplay: true,pauseOnHover: false,autoplaySpeed: 2000});var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-2')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-3')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-4')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-5')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-6')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-7')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-10')[0], "1.50s", "0.30s", 1, 0);
+wl.addAnimation($('.js-12')[0], "1.50s", "0.30s", 1, 0);
+wl.start();
+
+});
