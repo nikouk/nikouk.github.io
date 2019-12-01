85bbd8018dcfcc3f394a7d48d9bfb7a22ae37e75
nikouk <nikouk@nikouk.co.uk>
2019-12-01 18:42:02 +0000

    amity

--- js/amity.20191201184128.js
+++ js/amity.20191201184128.js
@@ -8,11 +8,13 @@ $('.js-13').attr('src', (dpi>1) ? ((dpi>2) ? 'images/cnv00094-1779.jpg' : 'image
 $('.js-14').attr('src', (dpi>1) ? ((dpi>2) ? 'images/esher-11-1200.jpg' : 'images/esher-11-800.jpg') : 'images/esher-11-400.jpg');
 $('.js-15').attr('src', (dpi>1) ? ((dpi>2) ? 'images/cnv00082-1779.jpg' : 'images/cnv00082-1186.jpg') : 'images/cnv00082-593.jpg');
 $('.js-16').attr('src', (dpi>1) ? ((dpi>2) ? 'images/esher-10-1200.jpg' : 'images/esher-10-800.jpg') : 'images/esher-10-400.jpg');
-$('.js-17').attr('src', (dpi>1) ? ((dpi>2) ? 'images/esher-14-1779.jpg' : 'images/esher-14-1186.jpg') : 'images/esher-14-593.jpg');
+$('.js-17').attr('src', (dpi>1) ? ((dpi>2) ? 'images/1stdec-4-1800.jpg' : 'images/1stdec-4-1200.jpg') : 'images/1stdec-4-600.jpg');
 $('.js-18').attr('src', (dpi>1) ? ((dpi>2) ? 'images/cnv00099-1200.jpg' : 'images/cnv00099-800.jpg') : 'images/cnv00099-400.jpg');
 $('.js-19').attr('src', (dpi>1) ? ((dpi>2) ? 'images/cnv00102-1779.jpg' : 'images/cnv00102-1186.jpg') : 'images/cnv00102-593.jpg');
-$('.js-21').attr('src', (dpi>1) ? ((dpi>2) ? 'images/street-1-573.jpg' : 'images/street-1-382.jpg') : 'images/street-1-191.jpg');
-$('.js-22').attr('src', (dpi>1) ? ((dpi>2) ? 'images/web-15-573.jpg' : 'images/web-15-382.jpg') : 'images/web-15-191.jpg');};
+$('.js-20').attr('src', (dpi>1) ? ((dpi>2) ? 'images/esher-14-1779.jpg' : 'images/esher-14-1186.jpg') : 'images/esher-14-593.jpg');
+$('.js-21').attr('src', (dpi>1) ? ((dpi>2) ? 'images/1stdec-2-1200.jpg' : 'images/1stdec-2-800.jpg') : 'images/1stdec-2-400.jpg');
+$('.js-23').attr('src', (dpi>1) ? ((dpi>2) ? 'images/street-1-573.jpg' : 'images/street-1-382.jpg') : 'images/street-1-191.jpg');
+$('.js-24').attr('src', (dpi>1) ? ((dpi>2) ? 'images/web-15-573.jpg' : 'images/web-15-382.jpg') : 'images/web-15-191.jpg');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 initMenu($('#menu-5')[0]);
